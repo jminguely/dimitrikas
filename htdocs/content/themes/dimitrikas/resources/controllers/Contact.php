3272352211dda182c489b00df44d06637ceb6b66
Julien Minguely <jminguely@gmail.com>
2018-11-29 00:16:46 +0100

    Fixes unnecessary backslash on form input

--- htdocs/content/themes/dimitrikas/resources/controllers/Contact.php
+++ htdocs/content/themes/dimitrikas/resources/controllers/Contact.php
@@ -32,7 +32,8 @@ class Contact extends Page
     static function ajaxProcess() {
         check_ajax_referer('dk', 'security');
         $data = array();
-        parse_str($_POST['data'], $data);
+        parse_str(wp_unslash($_POST['data']), $data);
+        print_r($data);
         $validated_data = MailProvider::validate($data);
         if ($validated_data) {
             echo MailProvider::send($data);
